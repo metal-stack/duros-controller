b65c62c125f09980e94ca495549de12d307e4945
Stefan Majer <stefan.majer@f-i-ts.de>
2021-03-05 15:32:27 +0100

    minor image update

--- controllers/images.go
+++ controllers/images.go
@@ -4,7 +4,7 @@ const (
 	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.4.0"
 	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.4.0"
 	csiProvisionerImage         = "k8s.gcr.io/sig-storage/csi-provisioner:v1.5.0"
-	csiAttacherImage            = "k8s.gcr.io/sig-storage/csi-attacher:v2.2.0"
+	csiAttacherImage            = "k8s.gcr.io/sig-storage/csi-attacher:v2.2.1"
 	csiResizerImage             = "k8s.gcr.io/sig-storage/csi-resizer:v0.5.0"
 	csiNodeDriverRegistrarImage = "k8s.gcr.io/sig-storage/csi-node-driver-registrar:v1.2.0"
 	snapshotControllerImage     = "k8s.gcr.io/sig-storage/snapshot-controller:v4.0.0"
