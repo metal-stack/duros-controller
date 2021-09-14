a427450332ae3dac0c9067a8ec53e73fe20ac2dc
Gerrit <info@gerritschwerthelm.de>
2021-09-14 16:07:49 +0200

    Not public.

--- controllers/duros_controller.go
+++ controllers/duros_controller.go
@@ -107,7 +107,7 @@ func (r *DurosReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 		return requeue, err
 	}
 
-	err = r.ReconcileStatus(ctx, &duros)
+	err = r.reconcileStatus(ctx, &duros)
 	if err != nil {
 		return requeue, err
 	}
@@ -115,7 +115,7 @@ func (r *DurosReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 	return ctrl.Result{}, nil
 }
 
-func (r *DurosReconciler) ReconcileStatus(ctx context.Context, duros *storagev1.Duros) error {
+func (r *DurosReconciler) reconcileStatus(ctx context.Context, duros *storagev1.Duros) error {
 	var (
 		updateTime = metav1.NewTime(time.Now())
 		ds         = &appsv1.DaemonSet{}
