3cb313beefd06076d8ca74916bb8cac060ec1d4d
Gerrit <info@gerritschwerthelm.de>
2021-09-23 14:12:54 +0200

    Remove finalizer from deletion flow to fix Gardener shoot deletion.

--- controllers/duros_controller.go
+++ controllers/duros_controller.go
@@ -26,7 +26,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	"github.com/metal-stack/duros-go"
@@ -89,28 +88,18 @@ func (r *DurosReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 	projectID := duros.Spec.MetalProjectID
 	storageClasses := duros.Spec.StorageClasses
 
-	if duros.ObjectMeta.DeletionTimestamp.IsZero() {
-		if !containsString(duros.GetFinalizers(), DurosFinalizerName) {
-			controllerutil.AddFinalizer(duros, DurosFinalizerName)
-			if err := r.Update(ctx, duros); err != nil {
-				return requeue, err
-			}
-		}
-	} else {
+	// FIXME: We should use finalizers here, however Gardener throws away
+	// all permissions of the controller at the same time as it deletes the
+	// duros resource. therefore, the cleanup cannot succeed and the cluster
+	// deletion hangs at 98% because the cr still has a finalizer attached.
+	if !duros.ObjectMeta.DeletionTimestamp.IsZero() {
 		// object is being deleted
 		// we don't pass the cancelled context here because then our deletion
 		// procedure will stop prematurely
 		deletionCtx := context.Background()
 
-		if containsString(duros.GetFinalizers(), DurosFinalizerName) {
-			if err := r.cleanupResources(deletionCtx); err != nil {
-				return requeue, err
-			}
-
-			controllerutil.RemoveFinalizer(duros, DurosFinalizerName)
-			if err := r.Update(deletionCtx, duros); err != nil {
-				return requeue, err
-			}
+		if err := r.cleanupResources(deletionCtx); err != nil {
+			return requeue, err
 		}
 
 		return ctrl.Result{}, nil
