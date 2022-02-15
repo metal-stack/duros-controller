047bc23817dcd6838ff6483e3757f00f795e2c69
Stefan Majer <stefan.majer@f-i-ts.de>
2022-02-15 07:36:52 +0100

    satisfy linter

--- controllers/resources.go
+++ controllers/resources.go
@@ -1022,15 +1022,16 @@ func (r *DurosReconciler) deployCSI(ctx context.Context, projectID string, scs [
 				obj.Parameters["compression"] = "enabled"
 			}
 			if sc.Encryption {
-				encryptionSecretName := "storage-encryption-key"
-				encryptionSecretNamespace := "${pvc.namespace}"
+				secretName := "storage-encryption-key"
+				//nolint:gosec
+				secretNamespace := "${pvc.namespace}"
 				obj.Parameters["encryption"] = "enabled"
-				obj.Parameters["csi.storage.k8s.io/controller-expand-secret-name"] = encryptionSecretName
-				obj.Parameters["csi.storage.k8s.io/controller-expand-secret-namespace"] = encryptionSecretNamespace
-				obj.Parameters["csi.storage.k8s.io/node-publish-secret-name"] = encryptionSecretName
-				obj.Parameters["csi.storage.k8s.io/node-publish-secret-namespace"] = encryptionSecretNamespace
-				obj.Parameters["csi.storage.k8s.io/node-stage-secret-name"] = encryptionSecretName
-				obj.Parameters["csi.storage.k8s.io/node-stage-secret-namespace"] = encryptionSecretNamespace
+				obj.Parameters["csi.storage.k8s.io/controller-expand-secret-name"] = secretName
+				obj.Parameters["csi.storage.k8s.io/controller-expand-secret-namespace"] = secretNamespace
+				obj.Parameters["csi.storage.k8s.io/node-publish-secret-name"] = secretName
+				obj.Parameters["csi.storage.k8s.io/node-publish-secret-namespace"] = secretNamespace
+				obj.Parameters["csi.storage.k8s.io/node-stage-secret-name"] = secretName
+				obj.Parameters["csi.storage.k8s.io/node-stage-secret-namespace"] = secretNamespace
 			}
 			return nil
 		})
