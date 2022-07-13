d5b6d677d8ba35a7d9f27e3cff05109f062abb5d
Stefan Majer <stefan.majer@f-i-ts.de>
2022-07-13 12:32:15 +0200

    try encryption csi driver

--- controllers/images.go
+++ controllers/images.go
@@ -1,8 +1,8 @@
 package controllers
 
 const (
-	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.8.0"
-	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.8.0"
+	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi-dev/lb-csi-plugin:pr16-120-5932e3a"
+	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi-dev/lb-csi-plugin:pr16-120-5932e3a"
 	csiProvisionerImage         = "k8s.gcr.io/sig-storage/csi-provisioner:v2.2.2"
 	csiAttacherImage            = "k8s.gcr.io/sig-storage/csi-attacher:v3.5.0"
 	csiResizerImage             = "k8s.gcr.io/sig-storage/csi-resizer:v1.5.0"
--- controllers/resources.go
+++ controllers/resources.go
@@ -1025,7 +1025,7 @@ func (r *DurosReconciler) deployCSI(ctx context.Context, projectID string, scs [
 				secretName := "storage-encryption-key"
 				//nolint:gosec
 				secretNamespace := "${pvc.namespace}"
-				obj.Parameters["encryption"] = "enabled"
+				obj.Parameters["hostEncryption"] = "enabled"
 				obj.Parameters["csi.storage.k8s.io/controller-expand-secret-name"] = secretName
 				obj.Parameters["csi.storage.k8s.io/controller-expand-secret-namespace"] = secretNamespace
 				obj.Parameters["csi.storage.k8s.io/node-publish-secret-name"] = secretName
