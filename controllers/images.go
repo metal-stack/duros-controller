b558276cf38098eea5e7871c68d1d6b3d49293d3
Stefan Majer <stefan.majer@f-i-ts.de>
2022-09-07 08:10:55 +0200

    Update csi driver to v1.9.0

--- controllers/images.go
+++ controllers/images.go
@@ -1,8 +1,8 @@
 package controllers
 
 const (
-	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.8.1"
-	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.8.1"
+	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.9.0"
+	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.9.0"
 	csiProvisionerImage         = "k8s.gcr.io/sig-storage/csi-provisioner:v2.2.2"
 	csiAttacherImage            = "k8s.gcr.io/sig-storage/csi-attacher:v3.5.0"
 	csiResizerImage             = "k8s.gcr.io/sig-storage/csi-resizer:v1.5.0"
