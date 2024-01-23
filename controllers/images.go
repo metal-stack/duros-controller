05f4c6629a4a90d38ece8450d86b4e33c371b9aa
Stefan Majer <stefan.majer@f-i-ts.de>
2024-01-23 13:37:51 +0100

    Updat lb-csi driver to v1.14.0 to get ubuntu support back

--- controllers/images.go
+++ controllers/images.go
@@ -1,8 +1,8 @@
 package controllers
 
 const (
-	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.13.0"
-	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.13.0"
+	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.14.0"
+	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.14.0"
 	csiProvisionerImage         = "registry.k8s.io/sig-storage/csi-provisioner:v3.6.3"
 	csiAttacherImage            = "registry.k8s.io/sig-storage/csi-attacher:v4.4.3"
 	csiResizerImage             = "registry.k8s.io/sig-storage/csi-resizer:v1.9.3"
