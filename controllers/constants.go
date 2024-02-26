86fcdcae47d8e7aeea0e99e5c722d7750e5dd9ff
Robert Volkmann <20912167+robertvolkmann@users.noreply.github.com>
2024-02-26 10:24:17 +0100

    Remove dependency metal-lib (#68)

---
+++ controllers/constants.go
@@ -0,0 +1,7 @@
+package controllers
+
+var (
+	metalClusterDescriptionTag = "cluster.metal-stack.io/description"
+
+	durosDoNotEditMessage = "DO NOT EDIT - This resource is managed by duros-controller. Any modifications are discarded and the resource is returned to the original state."
+)
