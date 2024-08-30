8752c0feb3f0c9e7138bc3c0b6fd9bd9583a690e
Stefan Majer <stefan.majer@f-i-ts.de>
2024-08-30 12:34:05 +0200

    Update lb-csi driver to 1.16.0 which fixes some mount issues (#81)

--- controllers/images.go
+++ controllers/images.go
@@ -1,12 +1,12 @@
 package controllers
 
 const (
-	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.15.0"
-	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.15.0"
-	csiProvisionerImage         = "registry.k8s.io/sig-storage/csi-provisioner:v3.6.4"
-	csiAttacherImage            = "registry.k8s.io/sig-storage/csi-attacher:v4.4.4"
-	csiResizerImage             = "registry.k8s.io/sig-storage/csi-resizer:v1.9.4"
-	csiNodeDriverRegistrarImage = "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.10.1"
-	snapshotControllerImage     = "registry.k8s.io/sig-storage/snapshot-controller:v6.3.4"
-	csiSnapshotterImage         = "registry.k8s.io/sig-storage/csi-snapshotter:v6.3.4"
+	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.16.0"
+	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.16.0"
+	csiProvisionerImage         = "registry.k8s.io/sig-storage/csi-provisioner:v5.0.2"
+	csiAttacherImage            = "registry.k8s.io/sig-storage/csi-attacher:v4.6.1"
+	csiResizerImage             = "registry.k8s.io/sig-storage/csi-resizer:v1.11.2"
+	csiNodeDriverRegistrarImage = "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.11.1"
+	snapshotControllerImage     = "registry.k8s.io/sig-storage/snapshot-controller:v7.0.2"
+	csiSnapshotterImage         = "registry.k8s.io/sig-storage/csi-snapshotter:v7.0.2"
 )
