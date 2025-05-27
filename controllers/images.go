ca84947616ca85c9eb4bd5b23ae70b62b8561440
Stefan Majer <stefan.majer@f-i-ts.de>
2025-05-27 13:16:23 +0200

    Lb csi v1.18 (#90)

--- controllers/images.go
+++ controllers/images.go
@@ -1,12 +1,12 @@
 package controllers
 
 const (
-	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.17.0"
-	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.17.0"
+	lbCSIPluginImage            = "docker.lightbitslabs.com/lightos-csi/lb-csi-plugin:1.18.0"
+	lbDiscoveryClientImage      = "docker.lightbitslabs.com/lightos-csi/lb-nvme-discovery-client:1.18.0"
 	csiProvisionerImage         = "registry.k8s.io/sig-storage/csi-provisioner:v5.2.0"
-	csiAttacherImage            = "registry.k8s.io/sig-storage/csi-attacher:v4.8.0"
-	csiResizerImage             = "registry.k8s.io/sig-storage/csi-resizer:v1.13.1"
+	csiAttacherImage            = "registry.k8s.io/sig-storage/csi-attacher:v4.8.1"
+	csiResizerImage             = "registry.k8s.io/sig-storage/csi-resizer:v1.13.2"
 	csiNodeDriverRegistrarImage = "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.13.0"
-	snapshotControllerImage     = "registry.k8s.io/sig-storage/snapshot-controller:v8.2.0"
-	csiSnapshotterImage         = "registry.k8s.io/sig-storage/csi-snapshotter:v8.2.0"
+	snapshotControllerImage     = "registry.k8s.io/sig-storage/snapshot-controller:v8.2.1"
+	csiSnapshotterImage         = "registry.k8s.io/sig-storage/csi-snapshotter:v8.2.1"
 )
